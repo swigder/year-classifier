36911ab1a1581841502628e03bdf9e0a94800eef
suri <swigder@gmail.com>
2017-12-06 21:04:39 +0100

    "support" for multilayer

--- sk_learn/model.py
+++ sk_learn/model.py
@@ -2,21 +2,24 @@ import numpy as np
 from sklearn import metrics
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.linear_model import SGDClassifier, SGDRegressor
+from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import Pipeline
 
 
 class Model:
     def __init__(self):
-        self.text_clf = Pipeline([('vect', CountVectorizer(max_df=.95, min_df=5)),
+        self.text_clf = Pipeline([('vect', CountVectorizer(max_df=.95, min_df=10)),
                                   ('tfidf', TfidfTransformer()),
-                                  ('clf', SGDClassifier(loss='squared_loss', penalty='l1',
-                                                        alpha=1e-3, random_state=42,
-                                                        max_iter=5, tol=None, class_weight='balanced',
-                                                        verbose=3)),
+                                  # ('clf', SGDClassifier(loss='squared_loss', penalty='l1',
+                                  #                       alpha=1e-3, random_state=42,
+                                  #                       max_iter=5, tol=None, class_weight='balanced',
+                                  #                       verbose=3)),
                                   # ('clf', SGDRegressor(loss='squared_loss', penalty='l1',
                                   #                      alpha=1e-3, random_state=42,
                                   #                      max_iter=5, tol=None,
                                   #                      verbose=3)),
+                                  ('clf', MLPClassifier(solver='lbfgs', alpha=1e-5,
+                                                        verbose=True))
                                   ])
         print(self.text_clf)
 
@@ -38,7 +41,12 @@ class Model:
     def visualize(self):
         print()
         v = list(self.text_clf.named_steps['vect'].vocabulary_.keys())
-        for target, coeffs in enumerate(self.text_clf.named_steps['clf'].coef_):
-            top = np.argsort(np.abs(coeffs))[-20:]
-            print(u"{}\n{}".format(target, u"\n".join([u"{} {}".format(v[i], coeffs[i]) for i in top])))
+        # for target, coeffs in enumerate(self.text_clf.named_steps['clf'].coef_):
+        #     top = np.argsort(coeffs)[-10:]
+        #     print(u"{}\n{}".format(target,
+        #                            u"\n".join([u"{} {}".format(v[i], coeffs[i]) for i in top])))
+        for target, coeffs in enumerate(self.text_clf.named_steps['clf'].coefs_[0].T):
+            top = np.argsort(coeffs)[-10:]
+            print(u"{}\n{}".format(target,
+                                   u"\n".join([u"{} {}".format(v[i], coeffs[i]) for i in top])))
         print()
