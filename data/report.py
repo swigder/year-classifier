6af37bee5978babc24f44ac03138d80afd942087
suri <swigder@gmail.com>
2017-12-08 20:50:37 +0100

    better reports for data

--- data/report.py
+++ data/report.py
@@ -11,6 +11,26 @@ parser.add_argument('out', type=str, help='directory for report')
 
 args = parser.parse_args()
 
+corpora_nice_names = {'aftonbladet': 'Aftonbladet',
+                      'attasidor': '8 SIDOR',
+                      'blekingsposten': 'Blekingsposten',
+                      'bollnastidning': 'Bollnäs tidning',
+                      'dalpilen': 'Dalpilen',
+                      'dn': 'Dagens Nyheter',
+                      'fahluweckoblad': 'Fahlu weckoblad',
+                      'goteborgsweckoblad': 'Goteborgs weckoblad',
+                      'gotlandstidning': 'Gotlands tidning',
+                      'gp': 'Göteborgsposten',
+                      'kalmar': 'Kalmar',
+                      'ordat': 'Ordat',
+                      'postochinrikestidning': 'Post- och inrikes tidning,',
+                      'press': 'Press',
+                      'runebergdiverse': 'Runeberg diverse tidningar',
+                      'ubkvtdagny': 'Dagny',
+                      'ubkvtidun': 'Idun',
+                      'webbnyheter': 'Webbnyheter',
+                      }
+
 corpora = defaultdict(dict)
 decades = set()
 
@@ -22,7 +42,10 @@ for filename in os.listdir(args.dir):
     corp = ''.join(filter(str.isalpha, filename[5:filename.index('.')]))
     if corp.startswith('kubhist'):
         corp = corp[len('kubhist'):]
-    corpora[corp][decade] = sum(1 for line in open(os.path.join(args.dir, filename)))
+    corp = corpora_nice_names[corp]
+    if decade not in corpora[corp]:
+        corpora[corp][decade] = 0
+    corpora[corp][decade] += sum(1 for line in open(os.path.join(args.dir, filename)))
 
 table = pd.DataFrame(columns=sorted(decades))
 
@@ -31,7 +54,14 @@ for corpus, hits in corpora.items():
         table.at[corpus, decade] = count
 table.loc['Total'] = table.sum(axis=0)
 
-table.to_csv(os.path.join(args.out, 'report.csv'))
+pd.options.display.float_format = '{:,.0f}'.format
+
+print(table[list(range(1770, 1830, 10))].dropna(how='all').to_csv(float_format='%.0g'))
+print(table[list(range(1830, 1890, 10))].dropna(how='all').to_csv(float_format='%.0g'))
+print(table[list(range(1890, 1950, 10))].dropna(how='all').to_csv(float_format='%.0g'))
+print(table[list(range(1950, 2020, 10))].dropna(how='all').to_csv(float_format='%.0g'))
+
+# table.to_csv(os.path.join(args.out, 'report.csv'))
 
 # for corpus, hits in corpora.items():
 #     plt.plot(*zip(*sorted(hits.items())), label=corpus)
