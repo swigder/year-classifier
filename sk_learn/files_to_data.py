8f9f9ca15f91cca6af362c1c54041e710702088e
suri <swigder@gmail.com>
2017-12-08 10:21:05 +0100

    adding options for min sample size and max number of samples

--- sk_learn/files_to_data.py
+++ sk_learn/files_to_data.py
@@ -22,21 +22,34 @@ def targets(data):
         return list(itertools.chain.from_iterable([[target] * len(items) for target, items in data.items()]))
 
 
-def read_dir_into_dict(data_dir):
+def read_dir_into_dict(data_dir, max_samples_per_period=None, min_sample_size=None):
     data_dict = defaultdict(list)
     for filename in os.listdir(data_dir):
         if not filename.endswith('.txt'):
             continue
         with codecs.open(data_dir + filename, 'r', 'utf-8') as file:
             year = int(file.readline())
-            lines = list(file.readlines())
+            if year < 1780:
+                continue
+            lines = []
+            current_line = ""
+            for line in file.readlines():
+                if max_samples_per_period and len(lines) > max_samples_per_period:
+                    break
+                if len(current_line) < min_sample_size and len(line) < min_sample_size:
+                    current_line += line
+                else:
+                    lines.append(current_line)
+                    current_line = line
+            lines.append(current_line)
             data_dict[year] = lines
     return data_dict
 
 
 def read_data(data_dir, max_samples_per_period, period_length, min_sample_size=None):
-    train = read_dir_into_dict(data_dir + '/training/')
-    test = read_dir_into_dict(data_dir + '/test/')
+    args = {'max_samples_per_period': max_samples_per_period, 'min_sample_size': min_sample_size}
+    train = read_dir_into_dict(data_dir + '/training/', **args)
+    test = read_dir_into_dict(data_dir + '/test/', **args)
 
     return Data(train=data_set(train), test=data_set(test))
 
