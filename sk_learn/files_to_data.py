cf27c9322c783f7f76139d475df369d91078a1e2
suri <swigder@gmail.com>
2017-12-13 11:27:02 +0100

    fixed bugs in validation set

--- sk_learn/files_to_data.py
+++ sk_learn/files_to_data.py
@@ -10,6 +10,10 @@ Data = namedtuple('Data', ['train', 'test'])
 DataSet = namedtuple('DataSet', ['inputs', 'targets'])
 
 
+def data_from_dicts(train, test, reusable=False):
+    return Data(train=data_set(train, reusable=reusable), test=data_set(test, reusable=reusable))
+
+
 def data_set(data, reusable=False):
     return DataSet(inputs=inputs(data, reusable), targets=targets(data))
 
@@ -23,13 +27,17 @@ def targets(data):
         return list(itertools.chain.from_iterable([[target] * len(items) for target, items in data.items()]))
 
 
-def split_data_set(data_set, training=.9):
-    cutoff = int(len(data_set.inputs) * training)
-    return DataSet(inputs=data_set.inputs[:cutoff], targets=data_set.targets[:cutoff]), \
-           DataSet(inputs=data_set.inputs[cutoff:], targets=data_set.targets[cutoff:])
+def split_data(data, percent=.9):
+    training = dict()
+    test = dict()
+    for year, lines in data.items():
+        cutoff = int(len(lines) * percent)
+        training[year] = lines[cutoff:]
+        test[year] = lines[:cutoff]
+    return training, test
 
 
-def read_dir_into_data_set(data_dir, reusable=False, max_samples_per_period=None, min_sample_size=None):
+def read_dir_into_data_set(data_dir, max_samples_per_period=None, min_sample_size=None):
     data_dict = defaultdict(list)
     for filename in os.listdir(data_dir):
         if not filename.endswith('.txt'):
@@ -50,20 +58,18 @@ def read_dir_into_data_set(data_dir, reusable=False, max_samples_per_period=None
                     current_line = line
             lines.append(current_line)
             data_dict[year] = lines
-    return data_set(data_dict, reusable)
+    return data_dict
 
 
 def read_data(data_dir, max_samples_per_period=None, min_sample_size=None, reusable_input=False, validation_set=False):
     args = {'max_samples_per_period': max_samples_per_period,
-            'min_sample_size': min_sample_size,
-            'reusable': reusable_input}
+            'min_sample_size': min_sample_size}
 
     if not validation_set:
         train = read_dir_into_data_set(data_dir + '/training/', **args)
         test = read_dir_into_data_set(data_dir + '/test/', **args)
-        return Data(train=train, test=test)
+        return data_from_dicts(train, test, reusable=reusable_input)
     else:
-        args['reusable'] = True
         full_data_set = read_dir_into_data_set(data_dir + '/training/', **args)
-        train, test = split_data_set(full_data_set)
-        return Data(train=train, test=test)
+        train, test = split_data(full_data_set)
+        return data_from_dicts(train, test, reusable=True)
